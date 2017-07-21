0bd23016f6d0d3159400c9d99e1632203b391da1
crimsondryad <crimsondryad@gmail.com>
2017-07-20 22:42:29 -0400

    created class

--- api.php
+++ api.php
@@ -7,10 +7,25 @@ function get_user($username) {
         die("Try again, loser ".mysqli_error);
     }
     
-    echo mysql_result($result);
+    return mysql_result($result);
     
 }
+$possible_url = array("get_username");
 
-
+if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
+{
+  switch ($_GET["action"])
+    {
+      case "get_user":
+        $value = get_user();
+        break;
+      case "get_app":
+        if (isset($_GET["id"]))
+          $value = get_app_by_id($_GET["id"]);
+        else
+          $value = "Missing argument";
+        break;
+    }
+}
 
 ?>
