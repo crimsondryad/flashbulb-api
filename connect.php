729d64a75c200b6d42240fa53fa367fb0ebb5c38
crimsondryad <crimsondryad@gmail.com>
2017-07-21 00:56:14 -0400

    trying

--- connect.php
+++ connect.php
@@ -9,6 +9,7 @@ $dbname = getenv('MYSQLCONNSTR_schema');
 
 // Create connection
 
+/*
 $con=mysqli_init();
 //mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);
 mysqli_real_connect($con, "innovation-terminators.mysql.database.azure.com", "terminators@innovation-terminators", $password, $dbname, 3306);
@@ -17,5 +18,25 @@ mysqli_real_connect($con, "innovation-terminators.mysql.database.azure.com", "te
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 } 
-echo "Connected successfully";
+*/
+<?php
+   
+$servername = getenv('MYSQLCONNSTR_host');
+$user = getenv('MYSQLCONNSTR_username');
+$password = getenv('MYSQLCONNSTR_password');
+$dbname = getenv('MYSQLCONNSTR_schema');
+
+//phpinfo();
+
+// Create connection
+
+$con = mysqli_connect("innovation-terminators.mysql.database.azure.com", "terminators@innovation-terminators", $password, $dbname);
+mysqli_select_db($con); or die("Unable to connect to database");
+
+// Check connection
+/*
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+} 
+*/
 ?>
