1ca077e8221ef5ac41ab2c0dc19afc69f93174b3
crimsondryad <crimsondryad@gmail.com>
2017-07-21 03:22:27 -0400

    kinda working

--- api.php
+++ api.php
@@ -2,36 +2,32 @@
 require_once("connect.php");
 $username = $_GET['username'];
 
-function get_user($username) {
-  $sql = "select * from users where users.username = 'hodor'";
-    $result = mysqli_query($con, $sql);
-    if(!$result) {
-        die("Try again, loser ".mysqli_error);
-    }
-    echo $result;
-   if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        print_r($row);
-    }
-} else {
-    echo "0 results";
-} 
-    return mysql_result($result);
-    
+function get_user($username, $db) {
+  $query = "select * from users where users.username='".$username."'";
+  $result = mysqli_query($db, $query);
+  $row = mysqli_fetch_array($result);
+    $json = "{";
+    foreach($row as $line) {
+        $json .= $line . "\n";
+      }
+      $json .= "\n\n";
+    $json .= "}";
+
+echo json_encode($row);
 }
-$possible_url = array("get_username");
+
+$possible_url = array("get_user","get_message");
 
 if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
 {
   switch ($_GET["action"])
     {
       case "get_user":
-        $value = get_user();
+        $value = get_user($_GET['username'],$db);
         break;
-      case "get_app":
-        if (isset($_GET["id"]))
-          $value = get_app_by_id($_GET["id"]);
+      case "get_messages":
+        if (isset($_GET["uid"]))
+          $value = get_messages_by_user($_GET["uid"],$db);
         else
           $value = "Missing argument";
         break;
--- connect.php
+++ connect.php
@@ -5,30 +5,9 @@ $user = getenv('MYSQLCONNSTR_username');
 $password = getenv('MYSQLCONNSTR_password');
 $dbname = getenv('MYSQLCONNSTR_schema');
 
-//phpinfo();
-
 // Create connection
-
-/*
-$con=mysqli_init();
-//mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);
-mysqli_real_connect($con, "innovation-terminators.mysql.database.azure.com", "terminators@innovation-terminators", $password, $dbname, 3306);
-
-// Check connection
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-
-//phpinfo();
-*/
-
-// Create connection - old skool
 @ $db = mysqli_connect("innovation-terminators.mysql.database.azure.com", "terminators@innovation-terminators", $password) or die ("Could not connect");
-mysql_select_db("flashbulb_dev", $db) or die("No db selected");
+mysqli_select_db($db, "flashbulb_dev") or die("No db selected");
 
-// Check connection
-/*
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-} 
-*/
+$GLOBALS['db'];
 ?>
